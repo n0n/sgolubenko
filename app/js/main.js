eb14c124bfb5e7d052fc6d8473aacaf442971bae
Daria <nabatova.darya@protonmail.com>
2020-05-06 17:26:03 +0300

    services and map all resolutions

--- app/js/main.js
+++ app/js/main.js
@@ -11,3 +11,39 @@ menuBtn.addEventListener('click', function (evt) {
     menu.classList.toggle('main-nav--closed');
     menu.classList.toggle('main-nav--opened');
 });
+
+$(function () {
+    $('.services__list').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        rows: 2,
+        arrows: false,
+        dots: true,
+        dotsClass: 'services-dots',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1279,
+                settings: {
+                    variableWidth: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    rows: 2,
+                    arrows: false,
+                    dots: true,
+                    dotsClass: 'services-dots'
+                },
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    rows: 2,
+                    arrows: false,
+                    dots: true,
+                    dotsClass: 'services-dots'
+                }
+            }
+        ]
+    });
+});
